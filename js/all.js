5da5f9db85006bb2bd1970d87eeac4b7736c4a69
李翊銘 <white11190709@gmail.com>
2019-08-31 18:46:22 +0800

    重新調整位置以及排版，加入動畫特效

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    let showSkill = false;
     $('.elTop').click(function(e) {
         e.preventDefault();
         let target = $(this).attr('href');
@@ -9,6 +9,7 @@ $(document).ready(function() {
     $(window).scroll(function(e) {
         /* Act on the event */
         let windowScrollTop = $(window).scrollTop();
+        let windowHeight = $(window).height();
         $('.elTop').each(function() {
             let target = $(this).attr('href');
             let targetPos = $(target).offset().top;
@@ -18,12 +19,31 @@ $(document).ready(function() {
                 $(this).addClass('active');
             }
         })
-    });
-    $('.animate').each(function(e) {
-        let thisPos = $(this).offset().top;
-        let windowScrollTop = $(window).scrollTop();
-        let windowHeight = $(window).height();
-    });
+        $('.animate-right').each(function(e) {
+            let thisPos = $(this).offset().top;
+            let windowScrollTop = $(window).scrollTop();
+            
+            if (thisPos <= (windowScrollTop+windowHeight)){
+                $(this).addClass('bounceInRight animated')
+            }
+        });
+        $('.animate-left').each(function(e) {
+            let thisPos = $(this).offset().top;
+            let windowScrollTop = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (thisPos <= (windowScrollTop+windowHeight)){
+                $(this).addClass('bounceInLeft animated')
+            }
+        });
+        let skillPosTop = $('#skills').offset().top;
+        if(skillPosTop+(windowHeight/2) <= (windowScrollTop+windowHeight) && !showSkill){
+            showSkill = true;
+            $('.progress-bar').each(function() {
+                let getWidth = $(this).data('progress');
+                $(this).css('width',getWidth+'%');
+            });
+        }
+    }); 
+
 
 });
-
